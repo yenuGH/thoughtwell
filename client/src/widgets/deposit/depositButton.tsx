9b65337171225d6947d8c9c21ec9b74aab331902
yenu <35312312+yenuGH@users.noreply.github.com>
2024-08-05 17:45:14 -0700

    very minimal mvp for depositing and withdrawing

--- client/src/widgets/deposit/depositButton.tsx
+++ client/src/widgets/deposit/depositButton.tsx
@@ -1,28 +1,49 @@
-import { Button, Modal } from "@mantine/core";
+import { Button, Modal, Textarea } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
 
 export default function Deposit() {
   const [opened, { open, close }] = useDisclosure(false);
-  const handleClicked = () => {
-    alert("Deposit clicked!");
-  };
+
+  const [thought, setThought] = useState("");
+
+  function depositThought() {
+    // soemthing to do with local storage
+    localStorage.setItem("thought", thought); 
+  }
 
   return (
     <>
+      <Modal opened={opened} onClose={close} title="Deposit a thought">
+        <Textarea
+          label="Thought"
+          placeholder="What's on your mind?"
+          aria-label="Deposit a thought"
+          style={{ width: "100%" }}
+          onChange={(event) => {
+            // Update thought
+            setThought(event.currentTarget.value);
+          }}
+        />
 
-      <Modal opened={opened} onClose={close} title="Authentication">
-        {/* Modal content */}
+        <Button
+          onClick={() => {
+            // Deposit thought
+            console.log(thought);
+
+            // deposit the thought into local storage here
+            depositThought();
+
+            close();
+          }}
+        >
+          Deposit Thought
+        </Button>
       </Modal>
 
-      <Button
-        size="xl"
-        color="teal"
-        variant="light"
-        onClick={open}
-      >
+      <Button size="xl" color="teal" variant="light" onClick={open}>
         Deposit
       </Button>
-
     </>
   );
 }
--- client/src/widgets/withdraw/withdrawButton.tsx
+++ client/src/widgets/withdraw/withdrawButton.tsx
@@ -1,18 +1,41 @@
-import { Button } from "@mantine/core";
+import { Button, Modal, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
 
 export default function Withdraw() {
+    const [opened, { open, close }] = useDisclosure(false);
   const handleClicked = () => {
     alert("Withdraw clicked!");
   };
 
   return (
-    <Button
-      size="xl"
-      color="red"
-      variant="light"
-      onClick={handleClicked}
-    >
-      Withdraw
-    </Button>
+    <>
+        <Modal opened={opened} onClose={close} title="Deposit a thought">
+        <Text>
+            {localStorage.getItem("thought")}
+        </Text>
+
+        <Button
+          onClick={() => {
+            // Deposit thought
+            console.log(localStorage.getItem("thought"));
+
+            // deposit the thought into local storage here
+            close();
+          }}
+        >
+          Close
+        </Button>
+      </Modal>
+
+        <Button
+        size="xl"
+        color="red"
+        variant="light"
+        onClick={open}
+        >
+        Withdraw
+        </Button>
+    </>
   );
 }
