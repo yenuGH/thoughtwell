9394b5f4a29146f9135af8cf511644129ab4b846
yenu <35312312+yenuGH@users.noreply.github.com>
2024-08-23 23:49:13 -0700

    changed orientation of user settings and welcome message

--- client/src/widgets/signOutGroup.tsx
+++ client/src/widgets/signOutGroup.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Button, UnstyledButton } from "@mantine/core";
+import { Button, Divider, Flex } from "@mantine/core";
 
 import { getAuth, onAuthStateChanged, signOut, User } from "firebase/auth";
 import { AnimatePresence, motion } from "framer-motion";
@@ -12,73 +12,85 @@ import { sessionController } from "../controllers/sessionController";
 import UserSettings from "./userSettings/userSettings";
 
 export default function SignOutGroup() {
-  const [currentUser, updateCurrentUser] = useState<User | null>(null);
-  const navigate = useNavigate();
+    const [currentUser, updateCurrentUser] = useState<User | null>(null);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        updateCurrentUser(user);
-      } else {
-        updateCurrentUser(null);
-      }
-    });
+    useEffect(() => {
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                updateCurrentUser(user);
+            } else {
+                updateCurrentUser(null);
+            }
+        });
 
-    return () => unsubscribe();
-  }, []);
+        return () => unsubscribe();
+    }, []);
 
-  const handleSignOut = () => {
-    firebaseController
-      .signOut()
-      .then(() => {
-        // Sign-out successful.
-        navigate("/"); // Navigate after successful sign-out
+    const handleSignOut = () => {
+        firebaseController
+            .signOut()
+            .then(() => {
+                // Sign-out successful.
+                navigate("/"); // Navigate after successful sign-out
 
-        // Clear the session token
-        sessionController.clearSession();
-      })
-      .catch((error) => {
-        // An error happened.
-        console.error("Sign-out error:", error);
-      });
-  };
+                // Clear the session token
+                sessionController.clearSession();
+            })
+            .catch((error) => {
+                // An error happened.
+                console.error("Sign-out error:", error);
+            });
+    };
 
-  return (
-    <>
-      <AnimatePresence>
-        <motion.div
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0, transition: { delay: 0.5 } }}
-          exit={{ opacity: 0, y: 20 }}
-        >
-          {currentUser ? (
-            <>
-              <UserSettings />
-              <p style={{ color: "white" }}>Welcome, {currentUser.email}</p>
-            </>
-          ) : (
-            <>
-              <p style={{ color: "white" }}>Welcome, Guest</p>
-              <motion.div
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                transition={{ type: "spring" }}
-              >
-                <Button
-                  variant="light"
-                  color="orange"
-                  size="xs"
-                  radius="sm"
-                  onClick={() => navigate("/")}
+    return (
+        <>
+            <AnimatePresence>
+                <motion.div
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0, transition: { delay: 0.5 } }}
+                    exit={{ opacity: 0, y: 20 }}
                 >
-                  Log in
-                </Button>
-              </motion.div>
-            </>
-          )}
-        </motion.div>
-      </AnimatePresence>
-    </>
-  );
+                    {currentUser ? (
+                        <>
+                            <Flex direction="row" >
+                                <p style={{ color: "white" }}>
+                                    Welcome, {currentUser.email}
+                                </p>
+
+                                <Divider 
+                                    orientation="vertical"
+                                    my="sm" 
+                                    size={10}
+                                    color="transparent"
+                                />
+
+                                <UserSettings />
+                            </Flex>
+                        </>
+                    ) : (
+                        <>
+                            <p style={{ color: "white" }}>Welcome, Guest</p>
+                            <motion.div
+                                whileHover={{ scale: 1.1 }}
+                                whileTap={{ scale: 0.9 }}
+                                transition={{ type: "spring" }}
+                            >
+                                <Button
+                                    variant="light"
+                                    color="orange"
+                                    size="xs"
+                                    radius="sm"
+                                    onClick={() => navigate("/")}
+                                >
+                                    Log in
+                                </Button>
+                            </motion.div>
+                        </>
+                    )}
+                </motion.div>
+            </AnimatePresence>
+        </>
+    );
 }
